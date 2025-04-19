53927c09ff85d8fe0374d67d9f514038ef7e3f2f
G_03 <130713943+G3programmer@users.noreply.github.com>
2025-04-19 11:05:20 -0300

    Update açaiteria.py

--- 2025/abril/trabalho/açaiteria.py
+++ 2025/abril/trabalho/açaiteria.py
@@ -17,6 +17,7 @@ while True:
 
     prod = int(input("Digite o número do seu pedido: "))
 
+#Aqui é a subopção de tamanho depois de escolher o que o usuário deseja
     if prod == 1:
         print("\nBoa escolha! Mas qual tamanho você gostaria?")
         print("----------------------")
@@ -26,17 +27,23 @@ while True:
         print("| 4)     Voltar      |")
         print("----------------------")
 
+
+# Se caso o usuário escolher errado, poderá voltar com a opção 4, se não, pode continuar e se for inválido retornará ao início
+
         tamanho = int(input("Digite o número do tamanho do seu pedido: "))
 
         if tamanho in [1, 2, 3]:
             quant = int(input("Perfeito! Agora digite a quantidade que você gostaria de pedir: "))
             precos = {1: 11.00, 2: 16.00, 3: 20.00}
+            
+            #Aqui ele calculará o preço conforme o tamanho e a quantidade
             subtotal = precos[tamanho] * quant
             total += subtotal
         elif tamanho == 4:
             continue
         else:
             print("Valor inválido. Tente novamente.")
+            continue
 
     elif prod == 2:
         print("\nBoa escolha! Mas qual tamanho você gostaria?")
@@ -52,12 +59,15 @@ while True:
         if tamanho in [1, 2, 3]:
             quant = int(input("Perfeito! Agora digite a quantidade que você gostaria de pedir:\n"))
             precos = {1: 9.00, 2: 14.00, 3: 18.00}
+            
+            #Aqui ele calculará o preço conforme o tamanho e a quantidade
             subtotal = precos[tamanho] * quant
             total += subtotal
         elif tamanho == 4:
             continue
         else:
-            print("Valor inválido. Tente novamente.")
+            print("\nValor inválido. Tente novamente.\n")
+            continue
 
     elif prod == 3:
         break
@@ -65,12 +75,13 @@ while True:
     else:
         print("Essa opção não existe! Por favor, tente novamente\n\n\n")
         continue
-    
+
+    # Aqui ele da a opção de pedir mais coisa se preferir, para concluir deverá negar e para pedir mais deverá aceitar
     continuar = input(f"\nGostaria de mais alguma coisa? (S/N)\nTotal até agora: R${total:.2f}\n").strip().upper()
-    if continuar == "N" or continuar =="n":
+    if continuar == "N":
         break
 
-# FORA DO WHILE: EXIBE TOTAL FINAL
+# Saiu do While ele agradecerá e mostrará o valor a pagar
 print(f"\n===== Pedido Finalizado =====\nTotal a pagar: R${total:.2f}\nObrigado pela preferência!\n")
 print("""
  ------------------:: . @@@@@@ ..::-------------------------- 
